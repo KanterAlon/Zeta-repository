2db97c1d8ba47c1cb3914e50d2f8f38078168ac4
ALON KANTER <95320323@ort>
2024-10-31 15:16:39 -0300

    footer

--- Zeta/obj/Debug/net8.0/Zeta.AssemblyInfo.cs
+++ Zeta/obj/Debug/net8.0/Zeta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zeta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13852d5ae8477b6ae17c45195b8ef615ed61747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a070a043aef58c4336dd3f09f5309e4d1da4a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zeta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zeta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
